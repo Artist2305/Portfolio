48b6d92274a7405585096f86dc0422495027eb72
Mateusz Szostek <szostekm230590@gmail.com>
2020-11-08 13:01:16 +0000

    Lastest On Blog
    Lastest On Blog section added

--- ClientApp/src/components/LastestOnBlog.js
+++ ClientApp/src/components/LastestOnBlog.js
@@ -1,12 +1,68 @@
 ﻿import React from 'react';
 import '../css/LastestOnBlog.css';
+import img_1 from '../svg/blog_1.svg'
+import img_2 from '../svg/blog_2.svg'
+import img_3 from '../svg/blog_3.svg'
 
 const LastestOnBlog = () => {
 
         return (
-            <React.Fragment>
-                
-            </React.Fragment >
+            <div className="lob-container">
+                <div className="section-title">
+                    <h2>LASTEST ON BLOG</h2>
+                </div>
+                <div className="blog-article">
+                    <div className="dec-wrap">
+                        <div className="decorative-square sq-1"></div>
+                    </div>
+                    <div className="col lob-img-container">
+                        <img src={img_1}></img>
+                    </div>
+                    <div className="col lob-text center-vert">
+                        <p>20 OCTOBER 2020</p>
+                        <h3>Blog Article - Test Data</h3>
+                        <p>CHECK LIVE</p>
+                    </div>
+                    <div className="lob-mob-text">
+                        <p>20 OCTOBER 2020</p>
+                        <p>CHECK LIVE</p>
+                    </div>
+                </div>
+                <div className="blog-article">
+                    <div className="dec-wrap">
+                        <div className="decorative-square sq-2"></div>
+                    </div>
+                    <div className="col lob-img-container">
+                        <img src={img_2}></img>
+                    </div>
+                    <div className="col lob-text center-vert">
+                        <p>20 OCTOBER 2020</p>
+                        <h3>Blog Article - Test Data</h3>
+                        <p>CHECK LIVE</p>
+                    </div>
+                    <div className="lob-mob-text">
+                        <p>20 OCTOBER 2020</p>
+                        <p>CHECK LIVE</p>
+                    </div>
+                </div>
+                <div className="blog-article">
+                    <div className="dec-wrap">
+                        <div className="decorative-square sq-3"></div>
+                    </div>
+                    <div className="col lob-img-container">
+                        <img src={img_3}></img>
+                    </div>
+                    <div className="col lob-text center-vert">
+                        <p>20 OCTOBER 2020</p>
+                        <h3>Blog Article - Test Data</h3>
+                        <p>CHECK LIVE</p>
+                    </div>
+                    <div className="lob-mob-text">
+                        <p>20 OCTOBER 2020</p>
+                        <p>CHECK LIVE</p>
+                    </div>
+                </div>
+            </div>
         );
 }
 
