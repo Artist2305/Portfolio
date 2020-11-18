a0e0fb0d9c293144bfdc5714db1df2b8cfe011cb
Mateusz Szostek <szostekm230590@gmail.com>
2020-11-18 22:29:42 +0000

    Squares Background Animation

--- ClientApp/src/components/Square.js
+++ ClientApp/src/components/Square.js
@@ -2,17 +2,26 @@
 import '../css/Square.css';
 
 const Square = (props) => {
-        return (
-            <div className="square" style={
+    return (
+        <div className="square-move">
+            <div className="square-rotate" style={
                 {
                     left: (props.item.left * props.modifier) + 'px',
-                    top: (props.item.top * props.modifier) + 'px',
-                    backgroundColor: props.item.color + '',
-                    width: (props.item.size * props.modifier) + 'px',
-                    height: (props.item.size * props.modifier) + 'px'
-                }
-            }>
-            </div >
+                    top: (props.item.top * props.modifier) + 'px'
+                }}>
+                <div className={"square-color square-flash" +
+                    (props.item.color == "#BC8ED9" ? " color-1" : "") +
+                    (props.item.color == "#F28E8E" ? " color-2" : "") +
+                    (props.item.color == "#5DAEEA" ? " color-3" : "")
+                } style={
+                    {
+                        width: (props.item.size * props.modifier) + 'px',
+                        height: (props.item.size * props.modifier) + 'px'
+                    }
+                }>
+                </div >
+            </div>
+        </div>
     );
 }
 
